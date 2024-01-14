b4ecfdf23a964122b22321d4c73f403fc8ea84f7
ymgyt <yamaguchi7073xtt@gmail.com>
2024-01-14 19:38:46 +0900

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "syndicationd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, fenix, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          # sha256 = pkgs.lib.fakeSha256;
+          sha256 = "sha256-SXRtAuO4IqNOQq+nLbrsDFbVk+3aVA8NNpSZsKlVH/8=";
+        };
+
+        craneLib = crane.lib.${system}.overrideToolchain rustToolchain;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          # pname and version required, so set dummpy values
+          pname = "syndicationd-workspace";
+          version = "0.1";
+
+          builtInputs = [ ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        syndtermCrate = craneLib.crateNameFromCargoToml {
+          cargoToml = ./syndterm/Cargo.toml;
+        };
+        syndterm = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          inherit (syndtermCrate) pname version;
+          cargoExtraArgs = "--package ${syndtermCrate.pname}";
+        });
+
+        checks = {
+          inherit syndterm;
+
+          clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--workspace -- --deny warnings";
+          });
+
+          fmt = craneLib.cargoFmt commonArgs;
+        };
+      in {
+        inherit checks;
+
+        packages.default = self.packages."${system}".syndterm;
+        packages.syndterm = syndterm;
+
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [
+            # cargo and rustc provided by default
+            just
+            cargo-nextest
+            graphql-client
+            nixfmt
+            rust-analyzer
+          ];
+        };
+      });
+}
