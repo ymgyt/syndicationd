1238cec1da1979c67f73cedc9d9c6b33712d86da
ymgyt <yamaguchi7073xtt@gmail.com>
2024-01-23 04:57:24 +0900

    test: add find website url

--- synd/src/types.rs
+++ synd/src/types.rs
@@ -46,12 +46,37 @@ impl Feed {
 
     /// Return website link to which feed syndicate
     pub fn website_url(&self) -> Option<&str> {
-        match self.feed.feed_type {
-            // find rel == alternate link
-            FeedType::Atom => self
-                .feed
-                .links
-                .iter()
+        link::find_website_url(self.r#type(), &self.feed.links)
+    }
+
+    // for debug
+    pub fn without_entries(&self) -> Self {
+        let mut clone = self.clone();
+        clone.feed.entries.clear();
+        clone
+    }
+}
+
+impl From<(String, feed_rs::model::Feed)> for Feed {
+    fn from(feed: (String, feed_rs::model::Feed)) -> Self {
+        Feed {
+            url: feed.0,
+            feed: feed.1,
+        }
+    }
+}
+
+mod link {
+    use feed_rs::model::{FeedType, Link};
+
+    pub fn find_website_url<'a>(
+        feed_type: FeedType,
+        links: impl IntoIterator<Item = &'a Link>,
+    ) -> Option<&'a str> {
+        let mut links = links.into_iter();
+        match feed_type {
+            // Find rel == alternate link
+            FeedType::Atom => links
                 .find(|link| link.rel.as_deref() == Some("alternate"))
                 .map(|link| link.href.as_str()),
 
@@ -61,20 +86,73 @@ impl Feed {
             // TODO
             FeedType::RSS0 => todo!(),
 
-            // use first one
-            FeedType::RSS1 | FeedType::RSS2 => {
-                assert!(self.feed.links.len() < 2);
-                self.feed.links.first().map(|link| link.href.as_str())
-            }
+            // Use the first link whose rel is not "self"
+            FeedType::RSS1 | FeedType::RSS2 => links
+                .find(|link| link.rel.as_deref() != Some("self"))
+                .map(|link| link.href.as_str()),
         }
     }
-}
 
-impl From<(String, feed_rs::model::Feed)> for Feed {
-    fn from(feed: (String, feed_rs::model::Feed)) -> Self {
-        Feed {
-            url: feed.0,
-            feed: feed.1,
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn rss_ignore_rel_self() {
+            let links = vec![
+                Link {
+                    href: "https://syndicationd.ymgyt.io/".into(),
+                    title: None,
+                    rel: None,
+                    media_type: None,
+                    href_lang: None,
+                    length: None,
+                },
+                Link {
+                    href: "https://syndicationd.ymgyt.io/atom.xml".into(),
+                    title: None,
+                    rel: Some("self".into()),
+                    media_type: None,
+                    href_lang: None,
+                    length: None,
+                },
+            ];
+
+            assert_eq!(
+                find_website_url(FeedType::RSS1, &links),
+                Some("https://syndicationd.ymgyt.io/")
+            );
+            assert_eq!(
+                find_website_url(FeedType::RSS2, &links),
+                Some("https://syndicationd.ymgyt.io/")
+            );
+        }
+
+        #[test]
+        fn atom_use_rel_alternate() {
+            let links = vec![
+                Link {
+                    href: "https://syndicationd.ymgyt.io/atom.xml".into(),
+                    title: None,
+                    rel: Some("self".into()),
+                    media_type: None,
+                    href_lang: None,
+                    length: None,
+                },
+                Link {
+                    href: "https://syndicationd.ymgyt.io/".into(),
+                    title: None,
+                    rel: Some("alternate".into()),
+                    media_type: None,
+                    href_lang: None,
+                    length: None,
+                },
+            ];
+
+            assert_eq!(
+                find_website_url(FeedType::Atom, &links),
+                Some("https://syndicationd.ymgyt.io/")
+            );
         }
     }
 }
