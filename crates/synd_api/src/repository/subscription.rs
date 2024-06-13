523f58219a833d9572c7025aa01d8907e60dc4b6
ymgyt <yamaguchi7073xtt@gmail.com>
2024-06-14 00:30:05 +0900

    refactor(api): remove unused code

--- crates/synd_api/src/repository/subscription.rs
+++ crates/synd_api/src/repository/subscription.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use async_trait::async_trait;
 
 use crate::repository::{self, types::SubscribedFeeds};
@@ -21,28 +19,4 @@ pub trait SubscriptionRepository: Send + Sync {
     ) -> RepositoryResult<()>;
 
     async fn fetch_subscribed_feeds(&self, _user_id: &str) -> RepositoryResult<SubscribedFeeds>;
-}
-
-#[async_trait]
-impl<T> SubscriptionRepository for Arc<T>
-where
-    T: SubscriptionRepository,
-{
-    async fn put_feed_subscription(
-        &self,
-        feed: repository::types::FeedSubscription,
-    ) -> RepositoryResult<()> {
-        self.put_feed_subscription(feed).await
-    }
-
-    async fn delete_feed_subscription(
-        &self,
-        feed: repository::types::FeedSubscription,
-    ) -> RepositoryResult<()> {
-        self.delete_feed_subscription(feed).await
-    }
-
-    async fn fetch_subscribed_feeds(&self, user_id: &str) -> RepositoryResult<SubscribedFeeds> {
-        self.fetch_subscribed_feeds(user_id).await
-    }
 }
