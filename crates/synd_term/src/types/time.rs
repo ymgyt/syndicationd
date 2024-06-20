36212b4b1d00855b0b4206a45714cc42359dfb8e
Yuta Yamaguchi <yamaguchi7073xtt@gmail.com>
2024-06-21 05:34:37 +0900

    feat(term): use local timezone when displaying time (#71)

--- crates/synd_term/src/types/time.rs
+++ crates/synd_term/src/types/time.rs
@@ -5,12 +5,28 @@ pub trait TimeExt {
     fn local_ymd_hm(&self) -> String;
 }
 
+#[cfg(feature = "integration")]
 impl TimeExt for Time {
     fn local_ymd(&self) -> String {
-        self.naive_local().format("%Y-%m-%d").to_string()
+        self.format("%Y-%m-%d").to_string()
     }
 
     fn local_ymd_hm(&self) -> String {
-        self.naive_local().format("%Y-%m-%d %H:%M").to_string()
+        self.format("%Y-%m-%d %H:%M (%:z)").to_string()
+    }
+}
+
+#[cfg(not(feature = "integration"))]
+impl TimeExt for Time {
+    fn local_ymd(&self) -> String {
+        self.with_timezone(&chrono::Local)
+            .format("%Y-%m-%d")
+            .to_string()
+    }
+
+    fn local_ymd_hm(&self) -> String {
+        self.with_timezone(&chrono::Local)
+            .format("%Y-%m-%d %H:%M (%:z)")
+            .to_string()
     }
 }
