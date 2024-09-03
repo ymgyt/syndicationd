225e007e3c421d30accb184ebc1829be5652acaa
ymgyt <yamaguchi7073xtt@gmail.com>
2024-09-03 17:29:43 +0900

    refactor(api): use tracing initiazer for initialization of the tracing subscriber

--- crates/synd_api/src/main.rs
+++ crates/synd_api/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 
 use fdlimit::Outcome;
 use synd_o11y::{
-    opentelemetry::OpenTelemetryGuard, tracing_subscriber::otel_metrics::metrics_event_filter,
+    opentelemetry::OpenTelemetryGuard, tracing_subscriber::initializer::TracingInitializer,
 };
 use synd_stdx::color::{is_color_supported, ColorSupport};
 use tracing::{error, info};
@@ -17,56 +17,22 @@ use synd_api::{
 };
 
 fn init_tracing(options: &ObservabilityOptions) -> Option<OpenTelemetryGuard> {
-    use synd_o11y::{opentelemetry::init_propagation, tracing_subscriber::audit};
-    use tracing_subscriber::{
-        filter::EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt as _, Layer as _,
-        Registry,
-    };
+    let ObservabilityOptions {
+        show_code_location,
+        show_target,
+        otlp_endpoint,
+        trace_sampler_ratio,
+    } = options;
 
-    let (otel_layer, otel_guard) = match options
-        .otlp_endpoint
-        .as_deref()
-        .filter(|s| !s.is_empty())
-        .map(|endpoint| {
-            synd_o11y::opentelemetry_layer(
-                endpoint,
-                config::app::NAME,
-                config::app::VERSION,
-                options.trace_sampler_ratio,
-            )
-        }) {
-        Some((otel_layer, otel_guard)) => (Some(otel_layer), Some(otel_guard)),
-        _ => (None, None),
-    };
-
-    let ansi = is_color_supported() == ColorSupport::Supported;
-    let show_src = options.show_code_location;
-    let show_target = options.show_target;
-
-    Registry::default()
-        .with(
-            fmt::Layer::new()
-                .with_ansi(ansi)
-                .with_timer(fmt::time::ChronoLocal::rfc_3339())
-                .with_file(show_src)
-                .with_line_number(show_src)
-                .with_target(show_target)
-                .with_filter(metrics_event_filter())
-                .and_then(otel_layer)
-                .with_filter(
-                    EnvFilter::try_from_env("SYND_LOG")
-                        .or_else(|_| EnvFilter::try_new("info"))
-                        .unwrap()
-                        .add_directive(audit::Audit::directive()),
-                ),
-        )
-        .with(audit::layer())
-        .init();
-
-    // Set text map propagator globally
-    init_propagation();
-
-    otel_guard
+    TracingInitializer::default()
+        .app_name(config::app::NAME)
+        .app_version(config::app::VERSION)
+        .otlp_endpoint(otlp_endpoint.clone())
+        .trace_sampler_ratio(*trace_sampler_ratio)
+        .enable_ansi(is_color_supported() == ColorSupport::Supported)
+        .show_code_location(*show_code_location)
+        .show_target(*show_target)
+        .init()
 }
 
 async fn run(
