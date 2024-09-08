2194e41044c9e103dadb0c70ee61740827c2d522
ymgyt <yamaguchi7073xtt@gmail.com>
2024-09-08 12:41:04 +0900

    feat(stdx): add mock feature

--- crates/synd_stdx/src/fs/mod.rs
+++ crates/synd_stdx/src/fs/mod.rs
@@ -2,12 +2,17 @@ use std::{fs::File, io, path::Path};
 
 pub mod fsimpl;
 
+#[cfg_attr(feature = "mock", mockall::automock)]
 pub trait FileSystem {
+    #[cfg_attr(feature = "mock", mockall::concretize)]
     fn create_dir_all<P: AsRef<Path>>(&self, path: P) -> io::Result<()>;
 
+    #[cfg_attr(feature = "mock", mockall::concretize)]
     fn create_file<P: AsRef<Path>>(&self, path: P) -> io::Result<File>;
 
+    #[cfg_attr(feature = "mock", mockall::concretize)]
     fn open_file<P: AsRef<Path>>(&self, path: P) -> io::Result<File>;
 
+    #[cfg_attr(feature = "mock", mockall::concretize)]
     fn remove_file<P: AsRef<Path>>(&self, path: P) -> io::Result<()>;
 }
