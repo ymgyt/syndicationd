1f3cb0d73f0d51dbe9a3d9ef1434af7fbf66accc
ymgyt <yamaguchi7073xtt@gmail.com>
2024-09-09 05:25:50 +0900

    ci: add workflow to run terraform plan

--- hosting/terraform/grafana/main.tf
+++ hosting/terraform/grafana/main.tf
@@ -1,6 +1,6 @@
 terraform {
 
-  required_version = ">= 1.8.3"
+  required_version = ">= 1.9.5"
 
   cloud {
     organization = "ymgyt"
--- hosting/terraform/grafana/variables.tf
+++ hosting/terraform/grafana/variables.tf
@@ -2,6 +2,7 @@ variable "grafana_url" {
   type        = string
   sensitive   = true
   description = "grafana instance url"
+  default     = "https://ymgyt.grafana.net"
 }
 
 variable "grafana_sa_token" {
