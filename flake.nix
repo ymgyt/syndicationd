63a8bd0c19745b50b4167c80931346ff03eec33d
ymgyt <yamaguchi7073xtt@gmail.com>
2025-01-06 11:08:41 +0900

    chore: remove nixpkgs follow from crane

--- flake.nix
+++ flake.nix
@@ -10,10 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    crane = {
-      url = "github:ipetkov/crane/v0.20.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane/v0.20.0";
 
     flake-utils.url = "github:numtide/flake-utils";
 
