553d8c9707b5a35c76ebc6cdf738630552c92f5e
ymgyt <yamaguchi7073xtt@gmail.com>
2025-09-27 17:55:56 +0900

    chore: bump nixpkgs from 24.11 to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "syndicationd";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     rust-overlay = {
